7495bfd2551ea46474ddff04972f6cc06c5cf55b
jay2466 <kumarjai2466@gmail.com>
2023-01-13 17:27:27 +0530

    Made State Changes

--- src/App.js
+++ src/App.js
@@ -14,15 +14,14 @@ function App() {
    
 
 
-  const generated_puzzle = newStartingBoard(10)[1];
 
 
 
   const [puzzle_values,setPuzzle_values] = React.useState(puzzle_generate())
-  console.log(generated_puzzle);
   let count = 0;
   function puzzle_generate()
   { 
+    const generated_puzzle = newStartingBoard(10)[1];
     let pz_v = []
     for(let k = 0;k<9;k+=3)
     {
@@ -64,17 +63,8 @@ function App() {
 
 function handleChange(id)
 {
-  console.log(id);
+  console.log(id)
 }
-
-
-
-
-
-
-
-
-
 
 
 
@@ -109,7 +99,7 @@ function handleChange(id)
     for(let j = 0;j<9;j++)
     {
       const a1 = box();
-      array.push(<div className='sub-grid'>{a1}</div>);
+      array.push(<div className='sub-grid' >{a1}</div>);
     }
     return(array)
   }
--- src/Board.js
+++ src/Board.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default function Board(props)
 {
     return(
-            props.value !== 0 ?<input type = "text" className='small-box' value={props.value}/> : <input type = "text" className='small-box' onChange={props.handleChange(props.id)} /> 
+            
+            props.value !== 0 ?<div><input type = "text" className='small-box' value={props.value}/> </div>: <div onChange={props.handleChange}><input type = "text" className='small-box' /></div> 
       );
     
 }
